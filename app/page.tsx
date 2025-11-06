2b575a40a7f25767f2f5a474c07c5b850c863933
Senthee1811 <esentheepan@gmail.com>
2025-11-06 23:28:27 +0530

    Added Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExploreBtn from "@/components/ExploreBtn";
 
 
 export default function Home() {
@@ -5,6 +6,21 @@ export default function Home() {
     <section>
      <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in one place.</p>
+
+     <ExploreBtn />
+
+     <div className="mt-20 space-y-7">
+      <h3>Featured Events</h3>
+
+      <ul>
+        {[1,2,3,4,5].map((event) => (
+          <li key={event}>Event {event}</li>
+
+        ))}
+      </ul>
+
+
+     </div>
     </section>
   );
 }
